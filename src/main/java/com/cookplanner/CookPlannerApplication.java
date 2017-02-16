7f1097ca193b1acd80148fe83c0e1be019a696ab
ouedki <yassineouedki_5@hotmail.com>
2017-02-16 11:26:24 -0600

    Sprin boot initializer

---
+++ src/main/java/com/cookplanner/CookPlannerApplication.java
@@ -0,0 +1,12 @@
+package com.cookplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookPlannerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/cookplanner/CookPlannerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cookplanner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CookPlannerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
