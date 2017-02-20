43c0608d7f1883a65007ea08b5d942d8ff751782
abdelmoughit2003 <laajali.abdelmoughit@gmail.com>
2017-02-20 15:36:22 -0600

    create a UserRecipes interface to get from data base json file

---
+++ src/main/java/com/cookplanner/repositories/UserRecipes.java
@@ -0,0 +1,17 @@
+package com.cookplanner.repositories;
+
+import com.cookplanner.models.UserRecipe;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Created by abdelmoughit on 2/20/2017.
+ */
+@Repository
+public interface UserRecipes extends CrudRepository<UserRecipe, Long> {
+    @Query("FROM UserRecipe u INNER JOIN u.recipe r WHERE u.user.id=?1")
+    public List<UserRecipe> listOfRecipes(long userId);
+}
