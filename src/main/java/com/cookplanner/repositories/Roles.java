72ba0552b5c58d3017291818924f47edc5ab4bc6
abdelmoughit2003 <laajali.abdelmoughit@gmail.com>
2017-02-22 15:04:00 -0600

    Roles repository

--- src/main/java/com/cookplanner/repositories/Roles.java
+++ src/main/java/com/cookplanner/repositories/Roles.java
@@ -4,14 +4,11 @@ import com.cookplanner.models.UserRole;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 /**
  * @author Created by Yassine on 2/19/17.
  */
-
-
 @Repository
 public interface Roles extends CrudRepository<UserRole, Long> {
     @Query("select ur.role from UserRole ur, User u where u.username=?1 and ur.userId = u.id")
