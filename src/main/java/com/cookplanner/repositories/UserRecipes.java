a8af873df10c3f934049b253d7c00a98388989c2
abdelmoughit2003 <laajali.abdelmoughit@gmail.com>
2017-02-22 15:08:52 -0600

    Create a UserRecipes interface to make a query and get all data for a UserRecipe

--- src/main/java/com/cookplanner/repositories/UserRecipes.java
+++ src/main/java/com/cookplanner/repositories/UserRecipes.java
@@ -1,17 +1,14 @@
 package com.cookplanner.repositories;
-
 import com.cookplanner.models.UserRecipe;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
-
 /**
- * Created by abdelmoughit on 2/20/2017.
+ * Created by abdelmoughit on 2/20/2017
  */
 @Repository
 public interface UserRecipes extends CrudRepository<UserRecipe, Long> {
-    @Query("FROM UserRecipe u INNER JOIN u.recipe r WHERE u.user.id=?1")
-    public List<UserRecipe> listOfRecipes(long userId);
+    @Query("select u  FROM UserRecipe u INNER JOIN u.recipe r WHERE u.user.id=?1")
+     List<UserRecipe> listOfRecipes(long userId);
 }
