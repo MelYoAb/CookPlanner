2f60bc25f605fa00f00259ed969512e240f86ace
Melanie Sarver <melanie.sarver@gmail.com>
2017-02-25 15:57:01 -0600

    JS
    Created a function for the login animation.

--- src/main/resources/static/JS/fullplate.js
+++ src/main/resources/static/JS/fullplate.js
@@ -2,3 +2,10 @@
  * @author Created by apple on 2/17/17.
  */
 
+$(document).ready(function(){
+    $('#sign-in').click(function() {
+        $('#sign-in').addClass('scale-out');
+        $('#sign-up').addClass('scale-out');
+        $('.login').removeClass('scale-out').addClass('scale-in');
+    })
+});
