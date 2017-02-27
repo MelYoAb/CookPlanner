441600f628ede1be1611040331a72c03b03986ed
Melanie Sarver <melanie.sarver@gmail.com>
2017-02-26 22:26:16 -0600

    JS
    Added materialize initializer

--- src/main/resources/static/JS/fullplate.js
+++ src/main/resources/static/JS/fullplate.js
@@ -3,9 +3,14 @@
  */
 
 $(document).ready(function(){
+
+    //controls the appearance of the login form
     $('#sign-in').click(function() {
         $('#sign-in').addClass('scale-out');
         $('#sign-up').addClass('scale-out');
         $('.login').removeClass('scale-out').addClass('scale-in');
     })
+
+    //required by materialize for dropdown menu on create recipe form
+    $('select').material_select();
 });
