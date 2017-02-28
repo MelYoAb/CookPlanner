b55a29d4c120258ceb1fdf04c7b9e522402a869f
Melanie Sarver <melanie.sarver@gmail.com>
2017-02-28 10:10:16 -0600

    Controller
    Changed mapping url for dashboard

--- src/main/java/com/cookplanner/services/SecurityConfiguration.java
+++ src/main/java/com/cookplanner/services/SecurityConfiguration.java
@@ -33,7 +33,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http
                 .formLogin()
                 .loginPage("/login")
-                .defaultSuccessUrl("/recipes") // user's home page, it can be any URL
+                .defaultSuccessUrl("/users/dashboard") // user's home page, it can be any URL
                 .permitAll() // Anyone can go to the login page
                 .and()
                 .authorizeRequests()
