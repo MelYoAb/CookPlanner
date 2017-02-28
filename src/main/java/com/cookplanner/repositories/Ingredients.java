7342b0a39161856e8ff5cdb28dbd366fe6f37af8
ouedki <yassineouedki_5@hotmail.com>
2017-02-28 09:45:27 -0600

    Interface to run the query to get all ingredients for logged in user's recipes

--- src/main/java/com/cookplanner/repositories/Ingredients.java
+++ src/main/java/com/cookplanner/repositories/Ingredients.java
@@ -10,5 +10,13 @@ import java.util.List;
  * @author Created by Yassine on 2/22/17.
  */
 public interface Ingredients extends CrudRepository<Ingredient, Long> {
-
+    /*@Query( "SELECT i FROM Ingredient i\n" +
+            "JOIN i.ingredientLists il\n" +
+            "JOIN il.recipe r\n" +
+            "JOIN r.userRecipe ur\n" +
+            "JOIN ur.user u\n" +
+            "WHERE (u.id=?1\n)" +
+            "GROUP BY i.id" )
+    List<Ingredient> findIndredientsPerLoggedinUser(long id);*/
+//    AND ur.date > NOW())
 }
