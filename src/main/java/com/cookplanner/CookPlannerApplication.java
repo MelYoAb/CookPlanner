a1e98221643b41cb1824bb5d4b75074093097b2c
Melanie Sarver <melanie.sarver@gmail.com>
2017-03-07 16:00:00 -0600

    Configured application for deployment

--- src/main/java/com/cookplanner/CookPlannerApplication.java
+++ src/main/java/com/cookplanner/CookPlannerApplication.java
@@ -2,11 +2,15 @@ package com.cookplanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CookPlannerApplication {
-
+public class CookPlannerApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(CookPlannerApplication.class, args);
+	}
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CookPlannerApplication.class);
 	}
 }
